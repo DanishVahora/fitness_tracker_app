caa360f6acc8ed52c0ce31cdd766abdfe8ced28e
DanishVahora <danishvhora785@gmail.com>
2024-09-10 00:05:23 +0530

    Change in Weight Screen

--- lib/screens/weightScreen/weightScreen.dart
+++ lib/screens/weightScreen/weightScreen.dart
@@ -1,7 +1,8 @@
-import 'package:fitness_app_live/models/DetailPageButton.dart';
-import 'package:fitness_app_live/models/DetailPageTitle.dart';
-import 'package:fitness_app_live/models/ListWheelViewScroller.dart';
+import 'package:fitness_app_live/constants/color.dart';
 import 'package:flutter/material.dart';
+
+import '../../models/DetailPageButton.dart';
+import '../../models/DetailPageTitle.dart';
 
 class WeightPage extends StatefulWidget {
   const WeightPage({super.key});
@@ -11,11 +12,14 @@ class WeightPage extends StatefulWidget {
 }
 
 class _WeightPageState extends State<WeightPage> {
+  double weight = 60.0;
+  // Default weight
   @override
   Widget build(BuildContext context) {
-    List<String> items = [];
-    for (int i = 140; i < 200; i++) {
-      items.add(i.toString()+" lb");
+    List<String> levels = [];
+
+    for (var i = 30; i <= 500; i++) {
+      levels.add(i.isEven ? "|" : "I");
     }
     var size = MediaQuery.of(context).size;
     return Scaffold(
@@ -31,23 +35,65 @@ class _WeightPageState extends State<WeightPage> {
         ),
         child: Column(
           children: [
-            DetailPageTitle(
-                text: 'This helps us to create a personlized plan for you',
-                title: "What is your Height?",
-                color: Colors.white),
+            const DetailPageTitle(
+              text: 'You can change this later in the settings',
+              title: "What is your Weight?",
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: size.height * 0.055,
+            ),
+            Text('$weight kg',
+                style: TextStyle(
+                  color: PrimaryColor,
+                  fontSize: size.height * 0.04,
+                  fontWeight: FontWeight.bold,
+                )),
             SizedBox(
               height: size.height * 0.055,
             ),
             SizedBox(
-              height: size.height * 0.5,
-              child: listwheelScrollView(items: items,),
+              height: size.height * 0.4,
+              child: RotatedBox(
+                quarterTurns: -1,
+                child: ListWheelScrollView(
+                    itemExtent: size.height * 0.036,
+                    magnification: 1.2,
+                    useMagnifier: true,
+                    overAndUnderCenterOpacity: 0.3,
+                    physics: const FixedExtentScrollPhysics(),
+                    controller: FixedExtentScrollController(
+                        initialItem: (weight * 2).round() - 1),
+                    onSelectedItemChanged: (index) {
+                      setState(() {
+                        weight = (index + 1) / 2;
+                      });
+                    },
+                    children: levels.map((level) {
+                      return RotatedBox(
+                        quarterTurns: 1,
+                        child: Text(level,
+                            style: TextStyle(
+                              color: PrimaryColor,
+                              fontSize: size.height * 0.08,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      );
+                    }).toList()),
+              ),
             ),
-            DetailPageButton(text: 'Next', onTap: () {
-              Navigator.pushNamed(context, '/goal');
-            }, showBackButton: true,
-              onBackTap: (){
+            const Spacer(),
+            DetailPageButton(
+              text: 'Next',
+              onTap: () {
+                // userProvider.setWeight(weight);
+                Navigator.pushNamed(context, '/height');
+              },
+              showBackButton: true,
+              onBackTap: () {
                 Navigator.pop(context);
-              },),
+              },
+            ),
           ],
         ),
       ),
